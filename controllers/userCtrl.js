90ec16ae0c30e4763fdf0241006f10be274f2ff3
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-03-15 12:42:52 -0700

    set up user registration, acesstokens and cookies

---
+++ controllers/userCtrl.js
@@ -0,0 +1,75 @@
+const Users = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userCtrl = {
+  register: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+
+      const user = await Users.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "The email already exists" });
+      }
+
+      if (password.length < 6) {
+        return res
+          .status(400)
+          .json({ msg: "Password must be at least 6 characters." });
+      }
+      /////////////////////////
+      // Password Encryption
+      const passwordHash = await bcrypt.hash(password, 10);
+      const newUser = new Users({
+        name,
+        email,
+        password: passwordHash,
+      });
+
+      /////////////////////////
+      // Save to Mongo
+      await newUser.save();
+
+      /////////////////////////
+      // Handle tokens for auth
+      const accessToken = createAccessToken({ id: newUser._id });
+      const refreshToken = createRefreshToken({ id: newUser._id });
+
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+      });
+
+      res.json({ accessToken });
+      
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  refreshToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshToken;
+      if (!rf_token) {
+        return res.status(400).json({ msg: "Please Login or Register" });
+      }
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(400).json({ msg: "Please Login or Register" });
+        const accessToken = createAccessToken({ id: user.id });
+        res.json({ accessToken });
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+const createAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
+};
+const createRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+};
+
+module.exports = userCtrl;
